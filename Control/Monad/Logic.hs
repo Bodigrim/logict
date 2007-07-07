432a5393b1cec8798a58dfbc9dc6c40018abef17
dan.doel <dan.doel@gmail.com>
2007-07-07 04:17:37 +0000

    initial import

---
+++ Control/Monad/Logic.hs
@@ -0,0 +1,178 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE UndecidableInstances #-}
+
+-------------------------------------------------------------------------
+-- |
+-- Module : Control.Monad.Logic
+-- Copyright :
+-- License   :
+--
+-- Maintainer :
+-- Stability  : experimental
+-- Portability : non-portable (generalized newtype deriving,
+--                              multi-parameter type classes)
+--
+-- A backtracking, logic programming monad.
+--
+--    Inspired by the paper
+--    /Backtracking, Interleaving, and Terminating
+--        Monad Transformers/
+--    Oleg Kiselyov, Chung-chieh Shan, Daniel P. Friedman, Amr Sabry
+--    (http://www.cs.rutgers.edu/~ccshan/logicprog/LogicT-icfp2005.pdf)
+--
+--    Heavily adapted from the implementation here:
+--    (http://okmij.org/ftp/packages/LogicT.tar.gz)
+-------------------------------------------------------------------------
+
+module Control.Monad.Logic (
+    module Control.Monad.Logic.Class,
+    -- * The Logic monad
+    Logic(),
+    runLogic,
+    observe,
+    observeMany,
+    observeAll,
+    -- * The LogicT monad trasnformer
+    LogicT(),
+    runLogicT,
+    observeT,
+    observeManyT,
+    observeAllT,
+    module Control.Monad,
+    module Control.Monad.Trans
+  ) where
+
+import Control.Applicative
+
+import Control.Monad
+import Control.Monad.Identity
+import Control.Monad.Trans
+
+import Control.Monad.Reader.Class
+import Control.Monad.State.Class
+
+import qualified Data.Foldable as F
+import qualified Data.Traversable as T
+
+import Control.Monad.Logic.Class
+
+-- An Applicative instance for Identity, as MTL lacks one, currently
+instance Applicative Identity where
+    pure = Identity
+    (Identity f) <*> (Identity a) = Identity (f a)
+
+type SK r a = a -> r -> r
+type FK a = a
+
+-------------------------------------------------------------------------
+-- | A monad transformer for performing backtracking computations
+--   layered over another monad 'm'
+newtype LogicT m a =
+    LogicT { unLogicT :: forall ans. SK (m ans) a -> FK (m ans) -> m ans }
+
+-------------------------------------------------------------------------
+-- | Extracts the first result from a LogicT computation,
+-- failing otherwise.
+observeT :: Monad m => LogicT m a -> m a
+observeT lt = unLogicT lt (const . return) (fail "No answer.")
+
+-------------------------------------------------------------------------
+-- | Extracts all the results from a LogicT computation.
+observeAllT :: Monad m => LogicT m a -> m [a]
+observeAllT m = unLogicT m (liftM . (:)) (return [])
+
+-------------------------------------------------------------------------
+-- | Extracts up to a given number of results from a LogicT computation.
+observeManyT :: Monad m => Int -> LogicT m a -> m [a]
+observeManyT n m
+    | n <= 0 = return []
+    | n == 1 = unLogicT m (\a _ -> return [a]) (return [])
+    | otherwise = unLogicT (msplit m) sk (return [])
+ where
+ sk Nothing _ = return []
+ sk (Just (a, m')) _ = (a:) `liftM` observeManyT (n-1) m'
+
+-------------------------------------------------------------------------
+-- | Runs a LogicT computation with the specified initial success and
+-- failure continuations.
+runLogicT :: LogicT m a -> (a -> m r -> m r) -> m r -> m r
+runLogicT = unLogicT
+
+-------------------------------------------------------------------------
+-- | The basic Logic monad, for performing backtracking computations
+--   returning values of type 'a'
+newtype Logic a = Logic { unLogic :: LogicT Identity a }
+        deriving (Functor, Applicative, Alternative, Monad, MonadPlus, MonadLogic)
+
+-------------------------------------------------------------------------
+-- | Extracts the first result from a Logic computation.
+observe :: Logic a -> a
+observe = runIdentity . observeT . unLogic
+
+-------------------------------------------------------------------------
+-- | Extracts all results from a Logic computation.
+observeAll :: Logic a -> [a]
+observeAll = runIdentity . observeAllT . unLogic
+
+-------------------------------------------------------------------------
+-- | Extracts up to a given number of results from a Logic computation.
+observeMany :: Int -> Logic a -> [a]
+observeMany i = runIdentity . observeManyT i . unLogic
+
+-------------------------------------------------------------------------
+-- | Runs a Logic computation with the specified initial success and
+-- failure continuations.
+runLogic :: Logic a -> (a -> r -> r) -> r -> r
+runLogic l s f = runIdentity $ unLogicT (unLogic l) si fi
+ where
+ si = fmap . s
+ fi = Identity f
+
+instance (Functor f) => Functor (LogicT f) where
+    fmap f lt = LogicT $ \sk fk -> unLogicT lt (sk . f) fk
+
+instance (Applicative f) => Applicative (LogicT f) where
+    pure a = LogicT $ \sk fk -> sk a fk
+    f <*> a = LogicT $ \sk fk -> unLogicT f (\g fk' -> unLogicT a (sk . g) fk') fk
+
+instance (Applicative f) => Alternative (LogicT f) where
+    empty = LogicT $ \_ fk -> fk
+    f1 <|> f2 = LogicT $ \sk fk -> unLogicT f1 sk (unLogicT f2 sk fk)
+
+instance (Monad m) => Monad (LogicT m) where
+    return a = LogicT $ \sk fk -> sk a fk
+    m >>= f = LogicT $ \sk fk -> unLogicT m (\a fk' -> unLogicT (f a) sk fk') fk
+    fail _ = LogicT $ \_ fk -> fk
+
+instance (Monad m) => MonadPlus (LogicT m) where
+    mzero = LogicT $ \_ fk -> fk
+    m1 `mplus` m2 = LogicT $ \sk fk -> unLogicT m1 sk (unLogicT m2 sk fk)
+
+instance MonadTrans LogicT where
+    lift m = LogicT $ \sk fk -> m >>= \a -> sk a fk
+
+instance (MonadIO m) => MonadIO (LogicT m) where
+    liftIO = lift . liftIO
+
+instance (Monad m) => MonadLogic (LogicT m) where
+    msplit m = lift $ unLogicT m ssk (return Nothing)
+     where
+     ssk a fk = return $ Just (a, (lift fk >>= reflect))
+
+instance F.Foldable Logic where
+    foldr f z l = runLogic l f z
+
+instance T.Traversable Logic where
+    traverse g l = runLogic l (\a ft -> cons <$> g a <*> ft) (pure mzero)
+     where cons a l = return a `mplus` l
+
+-- Needs undecidable instances
+instance (MonadReader r m) => MonadReader r (LogicT m) where
+    ask = lift ask
+    local f m = LogicT $ \sk fk -> unLogicT m ((local f .) . sk) (local f fk)
+
+-- Needs undecidable instances
+instance (MonadState s m) => MonadState s (LogicT m) where
+    get = lift get
+    put = lift . put
+
---
+++ Control/Monad/Logic/Class.hs
@@ -0,0 +1,184 @@
+
+-------------------------------------------------------------------------
+-- |
+-- Module : Control.Monad.Logic.Class
+-- Copyright :
+-- License   :
+--
+-- Maintainer :
+-- Stability  : experimental
+-- Portability : non-portable (generalized newtype deriving)
+--
+-- A backtracking, logic programming monad.
+--
+--    Inspired by the paper
+--    /Backtracking, Interleaving, and Terminating
+--        Monad Transformers/
+--    Oleg Kiselyov, Chung-chieh Shan, Daniel P. Friedman, Amr Sabry
+--    (http://www.cs.rutgers.edu/~ccshan/logicprog/LogicT-icfp2005.pdf)
+--
+--    Heavily adapted from the implementation here:
+--    (http://okmij.org/ftp/packages/LogicT.tar.gz)
+-------------------------------------------------------------------------
+
+module Control.Monad.Logic.Class (MonadLogic(..), reflect) where
+
+import Control.Monad
+
+import qualified Control.Monad.State.Lazy as LazyST
+import qualified Control.Monad.State.Strict as StrictST
+
+import Control.Monad.Reader
+
+import Data.Monoid
+import qualified Control.Monad.Writer.Lazy as LazyWT
+import qualified Control.Monad.Writer.Strict as StrictWT
+
+-------------------------------------------------------------------------------
+-- | Minimal implementation: msplit
+--
+-- /msplit/
+--   Attempts to split the computation, giving access to the first
+--   result. Satisfies the following laws:
+--   > msplit mzero                == return Nothing
+--   > msplit (return a `mplus` m) == return (Just (a, m))
+--
+-- /interleave/
+--   Fair disjunction. It is possible for a logical computation
+--   to have an infinite number of potential results, for instance:
+--
+--   > odds = return 1 `mplus` liftM (2+) odds
+--
+--   Such computations can cause problems in some circumstances. Consider:
+--
+--   > do x <- odds `mplus` return 2
+--   >    if even x then return x else mzero
+--
+--   Such a computation may never consider the 'return 2', and will
+--   therefore never terminate. By contrast, interleave ensures fair
+--   consideration of both branches of a disjunction
+-- 
+-- /(>>-)/
+--   Fair conjunction. Similarly to the previous function, consider
+--   the distributivity law for MonadPlus:
+--
+--   > (mplus a b) >>= k = (a >>= k) `mplus` (b >>= k)
+--
+--   If 'a >>= k' can backtrack arbitrarily many tmes, (b >>= k) may never
+--   be considered. (>>-) takes similar care to consider both branches of
+--   a disjunctive computation.
+--
+-- /ifte/
+--   Logical conditional. The equivalent of Prolog's soft-cut. If its
+--   first argument succeeds at all, then the results will be fed into
+--   the success branch. Otherwise, the failure branch is taken.
+--   satisfies the following laws:
+--
+--   > ifte (return a) th el           == th a
+--   > ifte mzero th el                == el
+--   > ifte (return a `mplus` m) th el == th a `mplus` (m >>= th)
+--
+-- /once/
+--   Pruning. Selects one result out of many. Useful for when multiple
+--   results of a computation will be equivalent, or should be treated as
+--   such.
+class (MonadPlus m) => MonadLogic m where
+    msplit     :: m a -> m (Maybe (a, m a))
+    interleave :: m a -> m a -> m a
+    (>>-)      :: m a -> (a -> m b) -> m b
+    ifte       :: m a -> (a -> m b) -> m b -> m b
+    once       :: m a -> m a
+
+    -- All the class functions besides msplit can be derived from msplit, if
+    -- desired
+    interleave m1 m2 = msplit m1 >>=
+                        maybe m2 (\(a, m1') -> return a `mplus` interleave m2 m1')
+
+    m >>- f = do Just (a, m') <- msplit m
+                 interleave (f a) (m' >>- f)
+
+    ifte t th el = msplit t >>= maybe el (\(a,m) -> th a `mplus` (m >>= th))
+
+    once m = do Just (a, _) <- msplit m
+                return a
+
+{-
+    -- Alternate implementation, from the paper.
+    interleave m1 m2 = do r <- msplit m1
+                          case r of
+                            Nothing       -> m2
+                            Just (a, m1') -> return a `mplus` interleave m2 m1'
+    m >>- f = do r <- msplit m
+                 case r of
+                   Nothing -> mzero
+                   Just (a, m') -> interleave (f a) (m' >>- f)
+    ifte t th el = do r <- msplit t
+                      case r of
+                        Nothing -> el
+                        Just (a, m) -> th a `mplus` (m >>= th)
+    once m = do r <- msplit m
+                case r of
+                  Nothing    -> mzero
+                  Just (a,_) -> return a
+-}
+
+-------------------------------------------------------------------------------
+-- | The inverse of msplit. Satisfies the following law:
+-- > msplit m >>= reflect == m
+reflect :: MonadLogic m => Maybe (a, m a) -> m a
+reflect Nothing = mzero
+reflect (Just (a, m)) = return a `mplus` m
+
+-- An instance of MonadLogic for lists
+instance MonadLogic [] where
+    msplit []     = return Nothing
+    msplit (x:xs) = return $ Just (x, xs)
+
+-- Some of these may be questionable instances. Splitting a transformer does
+-- not allow you to provide different input to the monadic object returned.
+-- So, for instance, in:
+--
+--  let Just (_, rm') = runReaderT (msplit rm) r
+--   in runReaderT rm' r'
+--
+-- The "r'" parameter will be ignored, as "r" was already threaded through the
+-- computation. The results are similar for StateT. However, this is likely not
+-- an issue as most uses of msplit (all the ones in this library, at least) would
+-- not allow for that anyway.
+instance (MonadLogic m) => MonadLogic (ReaderT e m) where
+    msplit rm = ReaderT $ \e -> do r <- msplit $ runReaderT rm e
+                                   case r of
+                                     Nothing -> return Nothing
+                                     Just (a, m) -> return (Just (a, lift m))
+
+instance (MonadLogic m) => MonadLogic (StrictST.StateT s m) where
+    msplit sm = StrictST.StateT $ \s ->
+                    do r <- msplit (StrictST.runStateT sm s)
+                       case r of
+                            Nothing          -> return (Nothing, s)
+                            Just ((a,s'), m) ->
+                                return (Just (a, StrictST.StateT (\_ -> m)), s')
+
+instance (MonadLogic m) => MonadLogic (LazyST.StateT s m) where
+    msplit sm = LazyST.StateT $ \s ->
+                    do r <- msplit (LazyST.runStateT sm s)
+                       case r of
+                            Nothing -> return (Nothing, s)
+                            Just ((a,s'), m) ->
+                                return (Just (a, LazyST.StateT (\_ -> m)), s')
+
+instance (MonadLogic m, Monoid w) => MonadLogic (StrictWT.WriterT w m) where
+    msplit wm = StrictWT.WriterT $
+                    do r <- msplit (StrictWT.runWriterT wm)
+                       case r of
+                            Nothing -> return (Nothing, mempty)
+                            Just ((a,w), m) ->
+                                return (Just (a, StrictWT.WriterT m), w)
+
+instance (MonadLogic m, Monoid w) => MonadLogic (LazyWT.WriterT w m) where
+    msplit wm = LazyWT.WriterT $
+                    do r <- msplit (LazyWT.runWriterT wm)
+                       case r of
+                            Nothing -> return (Nothing, mempty)
+                            Just ((a,w), m) ->
+                                return (Just (a, LazyWT.WriterT m), w)
