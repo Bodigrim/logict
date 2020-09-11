d3dc8b57c244fc804ffcc98bcf57cf1ee1d0cc5a
Mark Lemay <lemay@bu.edu>
2020-09-11 13:49:33 -0400

    Update Control/Monad/Logic.hs
    Co-authored-by: Bodigrim <andrew.lelechenko@gmail.com>

--- Control/Monad/Logic.hs
+++ Control/Monad/Logic.hs
@@ -100,7 +100,7 @@ observeT lt = unLogicT lt (const . return) (fail "No answer.")
 -- 
 -- @fmap (take 20) $ runExcept $ observeAllT nats@
 --
--- In geteral, if the underlying monad manages control flow then 
+-- In general, if the underlying monad manages control flow then 
 -- @observeAllT@ may be unproductive under infinite branching,
 -- and @observeManyT@ should be used instead.
 observeAllT :: Monad m => LogicT m a -> m [a]
