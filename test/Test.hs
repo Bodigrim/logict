c552bda0977ed39a6f13b8fd2f1c32c8c3af893d
Kevin Quick <kquick@galois.com>
2020-12-30 22:00:42 -0800

    Add tests for msplit laws for each monad instance.

--- test/Test.hs
+++ test/Test.hs
@@ -2,16 +2,19 @@
 
 module Main where
 
-import Test.Tasty
-import Test.Tasty.HUnit
+import           Test.Tasty
+import           Test.Tasty.HUnit
 
-import Control.Exception
-import Control.Monad.Except
-import Control.Monad.Identity
-import Control.Monad.Logic
-import Control.Monad.Reader
-import Data.Bifunctor ( first )
-import Data.Either
+import           Control.Exception
+import           Control.Monad.Except
+import           Control.Monad.Identity
+import           Control.Monad.Logic
+import           Control.Monad.Reader
+import qualified Control.Monad.State.Lazy as SL
+import qualified Control.Monad.State.Strict as SS
+import           Data.Bifunctor ( first )
+import           Data.Either
+import           Data.Maybe
 
 monadReader1 :: Assertion
 monadReader1 = assertEqual "should be equal" [5 :: Int] $
@@ -48,11 +51,15 @@ yieldWords = go
 
 main :: IO ()
 main = defaultMain $ testGroup "All"
+  [ testGroup "Monad Reader + env"
     [ testCase "Monad Reader 1" monadReader1
     , testCase "Monad Reader 2" monadReader2
     , testCase "Monad Reader 3" monadReader3
+    ]
 
-    , testCase "runIdentity all"  $ [0..4] @=? (take 5 $ runIdentity $ observeAllT nats)
+  , testGroup "Various monads"
+    [
+      testCase "runIdentity all"  $ [0..4] @=? (take 5 $ runIdentity $ observeAllT nats)
     , testCase "runIdentity many" $ [0..4] @=? (runIdentity $ observeManyT 5 nats)
     , testCase "observeAll"       $ [0..4] @=? (take 5 $ observeAll nats)
     , testCase "observeMany"      $ [0..4] @=? (observeMany 5 nats)
@@ -61,8 +68,11 @@ main = defaultMain $ testGroup "All"
 
     , testCase "manyT runExceptT" $ [0..4] @=?
       (fromRight [] $ runExcept (observeManyT 5 nats))
+    ]
 
-    , testCase "runLogicT multi" $ ["Hello world !"] @=?
+  , testGroup "Control.Monad.Logic tests"
+    [
+      testCase "runLogicT multi" $ ["Hello world !"] @=?
       let conc w o = ((w <> " ") <>) <$> o in
       (runLogicT (yieldWords ["Hello", "world"]) conc (pure "!"))
 
@@ -84,8 +94,70 @@ main = defaultMain $ testGroup "All"
 
     , testCase "observeMany multi" $ [5,3] @=? observeMany 2 odds5down
     , testCase "observeMany none" $ ([] :: [Integer]) @=? observeMany 2 mzero
+    ]
+
+  , testGroup "Control.Monad.Logic.Class tests"
+    [
+      testGroup "msplit laws"
+      [
+        testGroup "msplit mzero == return Nothing"
+        [
+          testCase "msplit mzero == return Nothing :: []" $
+          msplit mzero @=? return (Nothing :: Maybe (String, [String]))
+        , testCase "msplit mzero == return Nothing :: ReaderT" $
+          let z :: ReaderT Int [] String
+              z = mzero
+          in assertBool "ReaderT" $ null $ catMaybes $ runReaderT (msplit z) 0
+        , testCase "msplit mzero == return Nothing :: LogicT" $
+          let z :: LogicT [] String
+              z = mzero
+          in assertBool "LogicT" $ null $ catMaybes $ concat $ observeAllT (msplit z)
+        , testCase "msplit mzero == return Nothing :: strict StateT" $
+          let z :: SS.StateT Int [] String
+              z = mzero
+          in assertBool "strict StateT" $ null $ catMaybes $ SS.evalStateT (msplit z) 0
+        , testCase "msplit mzero == return Nothing :: lazy StateT" $
+          let z :: SL.StateT Int [] String
+              z = mzero
+          in assertBool "lazy StateT" $ null $ catMaybes $ SL.evalStateT (msplit z) 0
+        ]
+
+      , testGroup "msplit (return a `mplus` m) == return (Just a, m)" $
+        let sample = [1::Integer,2,3] in
+        [
+          testCase "msplit []" $ do
+            let op = sample
+                extract = fmap (fmap fst)
+            extract (msplit op) @?= [Just 1]
+            extract (msplit op >>= (\(Just (_,nxt)) -> msplit nxt)) @?= [Just 2]
+        , testCase "msplit ReaderT" $ do
+            let op = ask
+                extract = fmap fst . catMaybes . flip runReaderT sample
+            extract (msplit op) @?= [sample]
+            extract (msplit op >>= (\(Just (_,nxt)) -> msplit nxt)) @?= []
+        , testCase "msplit LogicT" $ do
+            let op :: LogicT [] Integer
+                op = foldr (mplus . return) mzero sample
+                extract = fmap fst . catMaybes . concat . observeAllT
+            extract (msplit op) @?= [1]
+            extract (msplit op >>= (\(Just (_,nxt)) -> msplit nxt)) @?= [2]
+        , testCase "msplit strict StateT" $ do
+            let op :: SS.StateT Integer [] Integer
+                op = (SS.modify (+1) >> SS.get `mplus` op)
+                extract = fmap fst . catMaybes . flip SS.evalStateT 0
+            extract (msplit op) @?= [1]
+            extract (msplit op >>= \(Just (_,nxt)) -> msplit nxt) @?= [2]
+        , testCase "msplit lazy StateT" $ do
+            let op :: SL.StateT Integer [] Integer
+                op = (SL.modify (+1) >> SL.get `mplus` op)
+                extract = fmap fst . catMaybes . flip SL.evalStateT 0
+            extract (msplit op) @?= [1]
+            extract (msplit op >>= \(Just (_,nxt)) -> msplit nxt) @?= [2]
+        ]
+      ]
 
     ]
+  ]
 
 safely :: IO Integer -> IO (Either String Integer)
 safely o = first (head . lines . show) <$> (try o :: IO (Either SomeException Integer))
